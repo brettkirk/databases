6e7bb89456aae17ab9de0820288fe62bc5841b4e
Hack Reactor Student <student@hackreactor.com>
2017-07-28 17:10:12 -0700

    create sql database

--- server/schema.sql
+++ server/schema.sql
@@ -1,17 +1,42 @@
+DROP DATABASE IF EXISTS chat;
+
 CREATE DATABASE chat;
 
 USE chat;
 
+DROP TABLE IF EXISTS messages;
+
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  `id` INTEGER AUTO_INCREMENT,
+  `room_id` INTEGER NULL,
+  `text` VARCHAR(255) NULL,
+  `user_id` INTEGER NULL,
+  `created_at` TIMESTAMP NULL,
+  PRIMARY KEY (`id`)
 );
 
+DROP TABLE IF EXISTS rooms;
+
+CREATE TABLE rooms (
+  `id` INTEGER AUTO_INCREMENT,
+  `room_name` VARCHAR(31) NULL,
+  PRIMARY KEY (`id`)
+);
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+  `id` INTEGER AUTO_INCREMENT,
+  `username` VARCHAR(63) NULL,
+  PRIMARY KEY (`id`)
+);
 /* Create other tables and define schemas for them here! */
+ALTER TABLE messages ADD FOREIGN KEY (room_id) REFERENCES rooms (`id`);
+ALTER TABLE messages ADD FOREIGN KEY (user_id) REFERENCES users (`id`);
 
 
 
 
 /*  Execute this file from the command line by typing:
- *    mysql -u root < server/schema.sql
+ *    mysql -u student < server/schema.sql
  *  to create the database and the tables.*/
-
