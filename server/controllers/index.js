b690fe894aa1c0420524a4afb4997c17eba7e709
Hack Reactor Student <student@hackreactor.com>
2017-07-29 10:54:32 -0700

    style check

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,11 +1,11 @@
 var models = require('../models');
-var db = require('../db')
+var db = require('../db');
 var mysql = require('mysql');
 
 module.exports = {
   messages: {
     get: function (req, res) {
-      res.send('hello world')
+      res.send('hello world');
     }, // a function which handles a get request for all messages
     post: function (req, res) {
       // models.messages.post(req, res);
@@ -14,16 +14,16 @@ module.exports = {
         password: 'plantlife',
         database: 'chat'
       });
-      dbConnection.connect(function(err){
-        if (err) throw err
+      dbConnection.connect(function(err) {
+        if (err) { throw err; }
         dbConnection.query('INSERT INTO messages (user_id, text, room_id) VALUES (?,?,?)', [1, req.body.message, 1], function(err, result) {
-          if (err) throw err
+          if (err) { throw err; }
           dbConnection.query('SELECT * FROM messages', function(err, results) {
-            if (err) throw err
+            if (err) { throw err; }
             console.log(results[0].text);
-          })
-        })
-      })
+          });
+        });
+      });
       res.end('much win!');
     } // a function which handles posting a message to the database
   },
@@ -33,30 +33,30 @@ module.exports = {
     get: function (req, res) {},
     post: function (req, res) {
       var dbConnection = mysql.createConnection({
-          user: 'root',
-          password: 'plantlife',
-          database: 'chat'
-        });
-        dbConnection.connect(function(err){
-          if (err) throw err
-          dbConnection.query('SELECT username FROM users', function(err, results) {
-            if (err) throw err
-            var flag = true;
-            for (var i = 0; i < results.length; i++) {
-              if (results[i].username === req.body.username) {
-                flag = false;
-              }
+        user: 'root',
+        password: 'plantlife',
+        database: 'chat'
+      });
+      dbConnection.connect(function(err) {
+        if (err) { throw err; }
+        dbConnection.query('SELECT username FROM users', function(err, results) {
+          if (err) { throw err; }
+          var flag = true;
+          for (var i = 0; i < results.length; i++) {
+            if (results[i].username === req.body.username) {
+              flag = false;
             }
-            if (flag) {
-            dbConnection.query('REPLACE INTO users (username) VALUES (?)', req.body.username, function(err, result) {
-              if (err) throw err
-              dbConnection.query('SELECT * FROM users', function(err, results) {
-                if (err) throw err
-              })
-            })
           }
-        })
-      })
+          if (flag) {
+            dbConnection.query('REPLACE INTO users (username) VALUES (?)', req.body.username, function(err, result) {
+              if (err) { throw err; }
+              dbConnection.query('SELECT * FROM users', function(err, results) {
+                if (err) { throw err; }
+              });
+            });
+          }
+        });
+      });
       res.end('much win!');
     }
   }
